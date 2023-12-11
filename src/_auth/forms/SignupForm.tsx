3895d5f96e2b8d3f79192af92b94444164d52324
Pedro Reis <128660097+PedroReoli@users.noreply.github.com>
2023-12-11 17:57:34 -0300

    End SignIn /SignUp
    Att para acabar com o signin e o sign up
    (Logo ainda nao ajustavel)

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -75,13 +75,13 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <img src="/assets/icons/logo.svg" alt="logo"    />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
-          Create a new account
+          Crie uma conta nova
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use snapgram, Please enter your details
+          Construa laços, compartilhe sorrisos.
         </p>
 
         <form
@@ -92,7 +92,7 @@ const SignupForm = () => {
             name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="shad-form_label">Name</FormLabel>
+                <FormLabel className="shad-form_label">Nome</FormLabel>
                 <FormControl>
                   <Input type="text" className="shad-input" {...field} />
                 </FormControl>
@@ -106,7 +106,7 @@ const SignupForm = () => {
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="shad-form_label">Username</FormLabel>
+                <FormLabel className="shad-form_label">Nome de Usuário</FormLabel>
                 <FormControl>
                   <Input type="text" className="shad-input" {...field} />
                 </FormControl>
@@ -134,7 +134,7 @@ const SignupForm = () => {
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormLabel className="shad-form_label">Senha</FormLabel>
                 <FormControl>
                   <Input type="password" className="shad-input" {...field} />
                 </FormControl>
@@ -146,19 +146,19 @@ const SignupForm = () => {
           <Button type="submit" className="shad-button_primary">
             {isCreatingAccount || isSigningInUser || isUserLoading ? (
               <div className="flex-center gap-2">
-                <Loader /> Loading...
+                <Loader /> Carregando
               </div>
             ) : (
-              "Sign Up"
+              "Junte-se a Nós"
             )}
           </Button>
 
           <p className="text-small-regular text-light-2 text-center mt-2">
-            Already have an account?
+            Possui uma conta?
             <Link
               to="/sign-in"
               className="text-primary-500 text-small-semibold ml-1">
-              Log in
+              Entre
             </Link>
           </p>
         </form>
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -6,3 +6,7 @@ export const SignupValidation = z.object({
     email: z.string().email({ message: 'Por favor, insira um endereço de e-mail válido' }),
     password: z.string().min(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
 });
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
+  });
