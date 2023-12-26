559b49557dc43cb1079be2ecb86b61a0b4030c05
Pedro Reis <128660097+PedroReoli@users.noreply.github.com>
2023-12-26 20:16:56 -0300

    Finalizando o Post

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -1,11 +1,12 @@
 import {
     // useQuery,
-    useMutation
-    // useQueryClient,
-    // useInfiniteQuery,
+    useMutation,
+    useQueryClient,
+    // useInfiniteQuery,    
 } from "@tanstack/react-query";
-import { createUserAccount ,signInAccount, signOutAccount } from "@/lib/appwrite/api";
-import { INewUser } from "@/types";
+import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
+import { createUserAccount ,signInAccount, signOutAccount,updatePost,createPost } from "@/lib/appwrite/api";
+import { INewPost, INewUser, IUpdatePost, /*IUpdateUser */ } from "@/types";
 
 
 export const useCreateUserAccount = () => {
@@ -24,3 +25,31 @@ export const useSignInAccount = () => {
       mutationFn: signOutAccount,
     });
   };
+
+  export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
+    });
+  };
+  
+  
+  export const useUpdatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: IUpdatePost) => updatePost(post),
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+        });
+      },
+    });
+  };
+  
+  
+  
